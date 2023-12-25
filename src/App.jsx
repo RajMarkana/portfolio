b8ee24cd88d749954603e6509d87535352404807
Raj Markana <rajmarkana1324@gmail.com>
2023-12-25 17:06:12 +0530

    change bg

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ function App() {
 
   return (
     <>
-      <div className='flex h-screen flex-wrap '>
+      <div className='flex h-screen flex-wrap'>
         <SideMenu/>
         <Home/>
        </div>
